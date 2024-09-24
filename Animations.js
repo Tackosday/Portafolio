d9737b543262efb67c3d8f20399dfe390be8fcba
JuanDavidEscalanteCastañeda-Campus <juandaesca5@hotmail.com>
2024-09-23 20:35:59 -0500

    feat: :dizzy: Finish Animation Typing
    the animation typing is complete

--- Animations.js
+++ Animations.js
@@ -1,24 +1,50 @@
-let Words = ["< Developer />", "< Word2 />", "< Tword3 />"]
+let Words = ["< Artist />","< Frontend />","< Backend />" ]
 
 document.addEventListener("DOMContentLoaded", () => {
+   
+   //Animation Typing Subtitle
     let interation = 1
-    
-    function typing(word){
-        console.log(word)
+    let timeWait = 4000;
+
+    typing(Words[0])
+
+    function typing(word) {
         let elementRaw = document.getElementsByClassName("subtitle")[0]
         // elementRaw.textContent = `${Words[interation]}`
-        // let i=0;
-        // for(i ; ; i++){}
+        let i = 0;
+        let inter = 0;
+        let actionText = "";
+        
+
+
+
+        for (i; i < word.length; i++) {
+            setTimeout(() => {
+                actionText = actionText + word[inter]
+                elementRaw.textContent = `${actionText}`
+                inter++
+                setTimeout(() => {
+                    if(inter > 1){
+                        actionText = actionText.slice(0,inter)
+                    }else if(inter == 1){
+                        actionText = "\u00A0";
+                    }
+                        elementRaw.textContent = actionText
+                        inter--
+                }, timeWait)
+            }, i * 50)
+        }
+
+
     }
 
-    function deleteTyping(Word){}
 
     function alternator() {
         typing(Words[interation])
-        interation == Words.length-1? interation=0 : interation++
+        interation == Words.length - 1 ? interation = 0 : interation++
     }
 
-    setInterval(alternator,2000)
+    setInterval(alternator, timeWait+(100*Words[interation].length))
 
 
 })
